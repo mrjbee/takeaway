540c582b549241af9f5b1a761f8309fb6b61a188
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-06-11 21:05:46 +0300

    Before popup

--- app/src/main/java/team/monroe/org/takeaway/view/HeaderItemViewPresenter.java
+++ app/src/main/java/team/monroe/org/takeaway/view/HeaderItemViewPresenter.java
@@ -80,8 +80,8 @@ public interface HeaderItemViewPresenter {
             ac_image = combine(
                     animateAppearance(rootView.findViewById(R.id.panel_images),
                             heightSlide(
-                                    (int)DisplayUtils.dpToPx(23, context.getResources()),
-                                    (int)DisplayUtils.dpToPx(35, context.getResources())))
+                                    (int)DisplayUtils.dpToPx(18, context.getResources()),
+                                    (int)DisplayUtils.dpToPx(25, context.getResources())))
                         .showAnimation(duration_constant(200), interpreter_overshot())
                         .hideAnimation(duration_constant(200), interpreter_accelerate(1f)),
 
