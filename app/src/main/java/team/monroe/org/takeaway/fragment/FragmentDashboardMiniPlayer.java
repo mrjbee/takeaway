a1815253081db4fc8ab5be43e2213db6b99a774c
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-07-07 13:56:11 +0300

    Add mini player layout and fragment

---
+++ app/src/main/java/team/monroe/org/takeaway/fragment/FragmentDashboardMiniPlayer.java
@@ -0,0 +1,10 @@
+package team.monroe.org.takeaway.fragment;
+
+import team.monroe.org.takeaway.R;
+
+public class FragmentDashboardMiniPlayer extends FragmentDashboardActivity{
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_mini_player;
+    }
+}
