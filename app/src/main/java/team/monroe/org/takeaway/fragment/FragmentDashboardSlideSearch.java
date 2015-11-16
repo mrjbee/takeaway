f3b3e8248965dcc0824fc4fad64c3a390ec1b972
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-11-16 20:41:33 +0200

    Replace mock search slide with no op under development slide

--- app/src/main/java/team/monroe/org/takeaway/fragment/FragmentDashboardSlideSearch.java
+++ app/src/main/java/team/monroe/org/takeaway/fragment/FragmentDashboardSlideSearch.java
@@ -1,11 +1,14 @@
 package team.monroe.org.takeaway.fragment;
 
+import team.monroe.org.takeaway.R;
+
 /**
  * Created by mrjbee on 6/11/15.
  */
-public class FragmentDashboardSlideSearch extends FragmentDashboardMockSlide {
+public class FragmentDashboardSlideSearch extends FragmentDashboardSlide {
+
     @Override
-    protected String getHelloString() {
-        return "Search";
+    protected int getLayoutId() {
+        return R.layout.fragment_dashboard_search_slide;
     }
 }
